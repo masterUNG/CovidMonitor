f62e0becca24e341ae0bc25454dde27c4718873b
masterUNG <phrombutr@gmail.com>
2021-04-20 13:54:43 +0700

    update 13.54

--- lib/states/create_account.dart
+++ lib/states/create_account.dart
@@ -94,8 +94,11 @@ class _CreateAccountState extends State<CreateAccount> {
       ),
       body: Center(
         child: SingleChildScrollView(
-                  child: Column(
+          child: Column(
             children: [
+              Container(width: 350,
+                child: Image.asset('images/account.png'),
+              ),
               buildName(),
               buildTitle(),
               buildRow(),
@@ -103,6 +106,14 @@ class _CreateAccountState extends State<CreateAccount> {
               buildPhone(),
               buildUser(),
               buildPassword(),
+              Container(
+                  width: 250,
+                  margin: EdgeInsets.symmetric(vertical: 16),
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.cloud_upload),
+                    label: Text('Create Account'),
+                  )),
             ],
           ),
         ),
@@ -112,42 +123,48 @@ class _CreateAccountState extends State<CreateAccount> {
 
   Row buildRow() {
     return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 150,
-                child: RadioListTile(
-                  value: 'male',
-                  groupValue: gender,
-                  onChanged: (value) {
-                    setState(() {
-                      gender = value;
-                    });
-                  },
-                  title: Text('Male'),
-                ),
-              ),
-              Container(
-                width: 150,
-                child: RadioListTile(
-                  value: 'female',
-                  groupValue: gender,
-                  onChanged: (value) {
-                    setState(() {
-                      gender = value;
-                    });
-                  },
-                  title: Text('Female'),
-                ),
-              ),
-            ],
-          );
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: 150,
+          child: RadioListTile(
+            value: 'male',
+            groupValue: gender,
+            onChanged: (value) {
+              setState(() {
+                gender = value;
+              });
+            },
+            title: Text('Male'),
+          ),
+        ),
+        Container(
+          width: 150,
+          child: RadioListTile(
+            value: 'female',
+            groupValue: gender,
+            onChanged: (value) {
+              setState(() {
+                gender = value;
+              });
+            },
+            title: Text('Female'),
+          ),
+        ),
+      ],
+    );
   }
 
   Container buildTitle() {
-    return Container(margin: EdgeInsets.only(top: 16),
-            width: 250,
-            child: Text('Gender :', style: TextStyle(fontWeight: FontWeight.bold,),),
-          );
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: Text(
+        'Gender :',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
   }
 }
